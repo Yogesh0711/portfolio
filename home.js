c9332a4d062101a4425880b97de2df4a8742d27a
Yogesh0711 <yogeshramya475@gmail.com>
2024-12-17 12:46:39 +0530

    Updated file

---
+++ home.js
@@ -0,0 +1,18 @@
+const images = [
+    "images/project1.png",
+    "images/project2.png",
+    "images/project3.png",
+    "images/project4.png",
+    "images/project5.png",
+    "images/project6.png",
+];
+
+let currentIndex = 0;
+const imageElement = document.getElementById('dynamicImage');
+
+function updateImage() {
+    currentIndex = (currentIndex + 1) % images.length; // Move to the next image in the array
+    imageElement.src = images[currentIndex]; // Update the image source
+}
+
+setInterval(updateImage, 8000);
