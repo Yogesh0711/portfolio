c4880370e077552347d84f9c0c696a2293ab705c
Yogesh0711 <yogeshramya475@gmail.com>
2024-12-17 13:05:59 +0530

    Updated file

--- home.js
+++ home.js
@@ -1,10 +1,10 @@
 const images = [
     "images/project5.png",
     "images/project6.png",
-    "images/project3.png",
+    "images/project2.png",
     "images/project4.png",
     "images/project1.png",
-    "images/project2.png",
+    "images/project3.png",
 ];
 
 let currentIndex = 0;
